18c76b6b0686e908c25752377684014d4a491aeb
binho1908 <fabiosenninhaddt@gmail.com>
2021-01-17 20:31:58 -0300

    adicionado o template do primeiro exercio, pasta index .html mudou para .php e criado a pasta de estilização para o primeiro exercicio

---
+++ exercicio.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-AU-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Exercício</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização de Exercício</h2>
+    </header>
+    
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+    </main>
+    
+    <footer class="rodape">
+    C0D3R & ALUNOS © <?= date('Y');?> 
+    </footer>
+</body>
+</html>
