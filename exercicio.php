d81742021382b7d19865b91d0e98e6a8a37e4dd7
binho1908 <fabiosenninhaddt@gmail.com>
2021-01-18 15:39:46 -0300

    removido modulo 2 ao 9 do arquivo index. html o estilo css foi mantido pois irei adiciona-los em breve, adicionado os 5 primeiros exercícios, a pasta basico onde tem alguns arquivos com o conceito básico do php (implementalção de html, implementação de css, comentários e codigo php abreviado)

--- exercicio.php
+++ exercicio.php
@@ -7,17 +7,23 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
     <title>Exercício</title>
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização de Exercício</h2>
     </header>
-    
+    <nav class="navegacao">
+        <a href=<?= "{$_GET['dir']}/{$_GET['file']}.php"?> class="verde">SEM FORMATAÇÃO</a>
+        <a href="index.php" class="vermelho">VOLTAR</a>
+    </nav>
     <main class="principal">
         <div class="conteudo">
-            
+            <?php
+                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
         </div>
     </main>
     
